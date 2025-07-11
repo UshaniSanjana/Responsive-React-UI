92eed624a2fd3b7c77b062318db32e532bc724f1
Ushani-Sanjana <ush.sanjana@gmail.com>
2025-07-11 19:34:37 +0530

    Implement responsive navibar using material UI

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -1,23 +1,56 @@
-import { AppBar, Button, Stack, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { TiThMenu } from "react-icons/ti";
 
 const MuiNavbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const menuItems = ["About", "Features", "Contact", "logout"];
+
   return (
     <AppBar sx={{ backgroundColor: "white", color: "black" }}>
       <Toolbar>
-        <Typography variant="h5" sx={{flexGrow:1}}>USHANI</Typography>
-        <Stack direction='row' spacing={2} sx={{display:{xs:'none', md:'flex'}}}>
-          <Button sx={{color:'black'}}>About</Button>
-          <Button sx={{color:'black'}}>Features</Button>
-          <Button sx={{color:'black'}}>Contact</Button>
-          <Button sx={{color:'black'}}>Logout</Button>
+        <Typography variant="h5" sx={{ flexGrow: 1 }}>
+          USHANI
+        </Typography>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ display: { xs: "none", md: "flex" } }}
+        >
+          {menuItems.map((item) => (
+            <Button sx={{ color: "black" }} key={item}>
+              {item}
+            </Button>
+          ))}
         </Stack>
-        {/* <Stack direction='column' spacing={2} sx={{display:{xs:'flex', md:'none'}}}>
-          <Button>About</Button>
-          <Button>Features</Button>
-          <Button>Contact</Button>
-          <Button>Logout</Button>
-        </Stack> */}
 
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <IconButton onClick={toggleDrawer(true)}>
+            <TiThMenu />
+          </IconButton>
+
+          <Drawer anchor='top' open={open} onClose={toggleDrawer(false)}>
+            {menuItems.map((item) => (
+              <Button sx={{ color: "black" }} key={item}>
+                {item}
+              </Button>
+            ))}
+          </Drawer>
+        </Box>
       </Toolbar>
     </AppBar>
   );
